03ee033eb9ddbe620fb6c4c251c67bf9731cbcae
supitalp2-png <supitalp2@gmail.com>
2026-06-06 18:40:01 +0200

    "Mise à jour index.php"

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  echo "<h1>Hechicero est en ligne !</h1>";
+  echo "<h1>Hechicero est en ligne bande de connards !</h1>";
   echo "<p>Serveur opérationnel et opérationnel.</p>";
   phpinfo();
 ?>
